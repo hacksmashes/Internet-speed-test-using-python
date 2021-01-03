1d49740a1ee868e4bf44597c493bdf934209a759
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-03 22:47:59 +0530

    Update internet speed test.py

--- internet speed test.py
+++ internet speed test.py
@@ -3,29 +3,3 @@ import speedtest
 ist=speedtest.Speedtest()
 print("Download speed : ",ist.download())
 print("Upload speed : ",ist.upload())
-
-
-
-        # output will be in the form of bytes
-
-
-
-
-        # 10 lakh bytes = 1Mbps
-
-
-        
-        # here the output is 90 lakh bytes, so our speed is 9Mbps (for downloading)
-
-
-
-
-        # here the output is 20 lakh bytes, so our speed is 2Mbps (for uploading)
-
-
-
-
-        # just ignore the values after the point ( . ) in the output
-
-
-
